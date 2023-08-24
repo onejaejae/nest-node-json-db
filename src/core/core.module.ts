25a97714958f25562436811c99d11deecbb3c705
wonjjee <yoteamo7@naver.com>
2023-08-24 14:04:49 +0900

    feat: TransformInterceptor

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -5,9 +5,11 @@ import { Post } from 'src/api/post/entity/post.entity';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { ErrorInterceptor } from './interceptor/error.interceptor';
 import { NodeJsonDBExceptionFilter } from './filter/node-json-db.exception.filter';
+import { TransformInterceptor } from './interceptor/apiResponse.interceptor';
 
 const interceptors: ClassProvider[] = [
   { provide: APP_INTERCEPTOR, useClass: ErrorInterceptor },
+  { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
 ];
 const filters: ClassProvider[] = [
   { provide: APP_FILTER, useClass: NodeJsonDBExceptionFilter },
